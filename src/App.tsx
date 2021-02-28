ae9705a8711b99b9d3fa4c471c34eb0d2c5b3eca
Juliano Tavares <juliano.tavaress@gmail.com>
2021-02-28 16:06:35 -0300

    added styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import {  ThemeProvider } from 'styled-components'
+
+import GlobalStyle from './styles/global' 
+import Header from './components/Header'
+import light from './styles/themes/light';
 
 function App() {
+
   return (
-    <div className="App">
-     
-    </div>
+    <ThemeProvider theme={light}>
+      <div className="App">
+        <GlobalStyle />
+      <Header />
+      </div>
+    </ThemeProvider>
   );
 }
 
